b2d9744c02b0b951e0798b54a41987d6c5af26d7
Ole Simen Nilsen <olesimen.nilsen@gmail.com>
2020-02-01 22:32:52 +0100

    Question 2

--- olesimen-nilsen-js1-ma2.js
+++ olesimen-nilsen-js1-ma2.js
@@ -2,3 +2,17 @@
 const myFunctionExpression = function() {
     console.log("Ole Simen");
 };
+
+// Question 2
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function() {
+    console.log("I was clicked");
+});
+
+/* document.querySelector(".btn").addEventListener("click", function() {
+    console.log("I was clicked");
+});
+
+
+Do we need to select the button using a variable first, or could we do like this?  */
